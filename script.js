fe44c9b6f9d4fe339ca64851ad781c4bef713bc6
Ghulam Mustafa <130176095+ghulammustafa06@users.noreply.github.com>
2024-07-28 17:27:38 +0000

    initial JS files setup

---
+++ script.js
@@ -0,0 +1,41 @@
+const clientId = "MY_ID";
+const clientSecret = "MY_SECRET_KEY";
+
+async function getAccessToken() {
+  const response = await fetch("https://accounts.spotify.com/api/token", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+      Authorization: "Basic " + btoa(clientId + ":" + clientSecret),
+    },
+    body: "grant_type=client_credentials",
+  });
+  const data = await response.json();
+  return data.access_token;
+}
+
+async function initPlayer() {
+  const accessToken = await getAccessToken();
+  console.log("Access Token:", accessToken);
+  const player = new Spotify.Player({
+    name: "Web Playback SDK Quick Start Player",
+    getOAuthToken: (cb) => {
+      cb(accessToken);
+    },
+  });
+
+    player.addListener('initialization_error', ({ message }) => { console.error(message); });
+    player.addListener('authentication_error', ({ message }) => { console.error(message); });
+    player.addListener('account_error', ({ message }) => { console.error(message); });
+    player.addListener('playback_error', ({ message }) => { console.error(message); });
+
+    player.addListener('player_state_changed', state => { console.log(state); });
+
+
+    player.addListener('ready', ({ device_id }) => {
+        console.log('Ready with Device ID', device_id);
+    });
+
+}
+
+initPlayer();
